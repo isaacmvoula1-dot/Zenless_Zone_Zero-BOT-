7b8bd1ce748c6fcd0bf544ffcce633791dbafead
isaacmvoula1-dot <isaacmvoula1@gmail.com>
2026-02-07 19:57:42 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './ZZZ/ZenlessZoneZero.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
